cad52c871b7c0cb299dbd6ed3294bbe4f1306d5c
vinaypalnati <vinaypalnati6174@gmail.com>
2021-03-16 13:35:38 +0530

    added shipping details

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from "@angular/core";
-
+import { HttpClient } from "@angular/common/http";
 @Injectable()
 export class CartService {
   items = [];
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
   addToCart(product) {
     this.items.push(product);
   }
   clearCart() {
     this.items = [];
     return this.items;
+  }
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      "/assets/shipping.json"
+    );
   }
   getItems() {
     return this.items;
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { CartService } from "../cart.service";
-
+import { HttpClient } from "@angular/common/http";
 @Component({
   selector: "app-cart",
   templateUrl: "./cart.component.html",
