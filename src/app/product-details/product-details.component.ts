8104887ff800a14318c01d2219d19e09c229caf2
vinaypalnati <vinaypalnati6174@gmail.com>
2021-03-16 12:11:35 +0530

    New Routing and Navigation

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -7,7 +7,7 @@ import { products } from "../products";
   styleUrls: ["./product-details.component.css"]
 })
 export class ProductDetailsComponent implements OnInit {
-  product:any;
+  product;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
