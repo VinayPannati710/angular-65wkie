22940b3d5782b5ef81b645cce33943d60b332a54
vinaypalnati <vinaypalnati6174@gmail.com>
2021-03-16 14:15:05 +0530

    checkoutform

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from "@angular/core";
 import { CartService } from "../cart.service";
 import { HttpClient } from "@angular/common/http";
+import { FormBuilder } from "@angular/forms";
 @Component({
   selector: "app-cart",
   templateUrl: "./cart.component.html",
   styleUrls: ["./cart.component.css"]
 })
 export class CartComponent {
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+  checkoutForm = this.formBuilder.group({
+    name: "",
+    address: ""
+  });
   items = this.cartService.getItems();
+  onSubmit(): void {
+    this.cartService.clearCart();
+    console.warn("Your order has been submitted", this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
